027ef992392f4731b0f0494f3f7824c30d5731ac
Júlio Nepomuceno <julioreplay@gmail.com>
2024-06-01 11:59:08 -0300

    [feature]: generate refreshToken

--- sso/src/main/java/com/easy_station/sso/auth/RefreshTokenRepository.java
+++ sso/src/main/java/com/easy_station/sso/auth/RefreshTokenRepository.java
@@ -2,6 +2,9 @@ package com.easy_station.sso.auth;
 
 import com.easy_station.sso.auth.domain.RefreshToken;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRepository extends MongoRepository<RefreshToken, String> {
+    RefreshToken findByUserId(String userId);
 }
--- sso/src/main/java/com/easy_station/sso/auth/domain/RefreshToken.java
+++ sso/src/main/java/com/easy_station/sso/auth/domain/RefreshToken.java
@@ -1,8 +1,6 @@
 package com.easy_station.sso.auth.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,6 +8,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.Instant;
 
 @Document
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
@@ -23,7 +23,7 @@ public class RefreshToken {
     @CreatedDate
     private Instant createdAt;
 
-    public RefreshToken( Instant expiresIn, String userId) {
+    public RefreshToken(Instant expiresIn, String userId) {
         this.expiresIn = expiresIn;
         this.userId = userId;
         this.createdAt = Instant.now();
